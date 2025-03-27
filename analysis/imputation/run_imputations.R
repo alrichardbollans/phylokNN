d076b446f4a1d3d7655eb2e1d350c12f834848b9
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2025-03-27 22:21:58 +0000

    Add multithreading

--- analysis/imputation/run_imputations.R
+++ analysis/imputation/run_imputations.R
@@ -1,47 +1,53 @@
-repo_path = Sys.getenv('KEWSCRATCHPATH')
-source(file.path(repo_path, 'phyloKNN', 'analysis', 'imputation','R_continuous_imputation_helper_functions.R'))
+library(doParallel)
+library(foreach)
+
+num_cores <- detectCores() - 1  # Use all but one core
+cl <- makeCluster(num_cores)
+registerDoParallel(cl)
+
+
+
 number_of_simulation_iterations = 100
-missingness_types = c('mcar', 'phyloNa')
-
-
-for (iter in c(1:number_of_simulation_iterations)) {
+foreach(iter = 1:number_of_simulation_iterations) %dopar% {
+  repo_path = Sys.getenv('KEWSCRATCHPATH')
+  source(file.path(repo_path, 'phyloKNN', 'analysis', 'imputation','R_continuous_imputation_helper_functions.R'))
+  
+  missingness_types = c('phyloNa')
+  
   print(iter)
-  for(missing_type in missingness_types){
+  for (missing_type in missingness_types) {  # Keep the inner loop sequential
+    
     
     # Binary cases
-    run_picante_models('BISSE', 'binary', iter, missing_type)
-    run_picante_models('HISSE', 'binary', iter, missing_type)
-    run_corHMM_models('BISSE', 'binary', iter, missing_type)
+    # run_picante_models('BISSE', 'binary', iter, missing_type)
+    # run_picante_models('HISSE', 'binary', iter, missing_type)
+    # run_corHMM_models('BISSE', 'binary', iter, missing_type)
     run_corHMM_models('HISSE', 'binary', iter, missing_type)
     
-    # Continuous cases
-    run_phylopars_models('BMT', 'continuous', iter, missing_type)
-    run_phylopars_models('EB', 'continuous', iter, missing_type)
-    
-    run_picante_models('BMT', 'continuous', iter, missing_type)
-    
-    run_picante_models('EB', 'continuous', iter, missing_type)
-  }
-}
-  
-for (iter in c(2:27)){ #1:number_of_simulation_iterations)) {
-  print(iter)
-  for(missing_type in missingness_types){
     
     # run_picante_models('simulations', 'binary', iter, missing_type)
     # run_corHMM_models('simulations', 'binary', iter, missing_type)
-    run_phylopars_models('simulations', 'continuous', iter, missing_type)
-    run_picante_models('simulations', 'continuous', iter, missing_type)
+    
+    
+    # Continuous cases
+    # run_phylopars_models('simulations', 'continuous', iter, missing_type)
+    # run_picante_models('simulations', 'continuous', iter, missing_type)
+    # run_phylopars_models('BMT', 'continuous', iter, missing_type)
+    # run_phylopars_models('EB', 'continuous', iter, missing_type)
+    # run_picante_models('BMT', 'continuous', iter, missing_type)
+    # run_picante_models('EB', 'continuous', iter, missing_type)
+    
+    
   }
 }
 
 
 
 
-# With real data takes far too long
-run_picante_models('real_data', 'binary', 1, 'mcar')
-# run_corHMM_models('real_data', 'binary', 1, 'mcar')
-# run_phylopars_models('real_data', 'continuous', 1, 'mcar')
-# run_picante_models('real_data', 'continuous', 1, 'mcar')
-print('Not implemented')
- 
+# # With real data takes far too long
+# run_picante_models('real_data', 'binary', 1, 'mcar')
+# # run_corHMM_models('real_data', 'binary', 1, 'mcar')
+# # run_phylopars_models('real_data', 'continuous', 1, 'mcar')
+# # run_picante_models('real_data', 'continuous', 1, 'mcar')
+# print('Not implemented')
+#  
