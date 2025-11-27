a69651fe800ffbc2c04aa69fbf982b6f1eaef776
alrichardbollans <38588335+alrichardbollans@users.noreply.github.com>
2025-11-27 17:09:55 +0000

    Add ttests for weak signal

--- analysis/evaluation/analyse_signal.py
+++ analysis/evaluation/analyse_signal.py
@@ -7,6 +7,7 @@ from analysis.evaluation.compare_types import plot_binary_and_continuous_cases
 from analysis.evaluation.compile_prediction_scores import read_all_results
 import seaborn as sns
 
+from analysis.evaluation.do_ttests_for_results import ttests
 from analysis.evaluation.helper_functions import bin_model_names, cont_model_names, rename_models_and_ev_models
 
 
@@ -114,46 +115,49 @@ def main():
     pathlib.Path(out_dir).mkdir(parents=True, exist_ok=True)
 
     bin_df, cont_df = read_all_results()
-    # plot signal distribution
-    sns.displot(bin_df.reset_index(drop=True), x='Signal', kind='kde')
-    plt.savefig(os.path.join(out_dir, f'D.jpg'), dpi=300)
-    plt.close()
-
-    sns.displot(cont_df, x='Signal', kind='kde')
-    plt.savefig(os.path.join(out_dir, f'lambda.jpg'), dpi=300)
-    plt.close()
-
-    ## plot performance against signal for each model
-    for model in bin_model_names:
-        plot_signal_performance(bin_df, model, 'binary', os.path.join(out_dir, 'binary models', model))
-        plot_signal_performance(bin_df, model, 'binary', os.path.join(out_dir, 'binary models', model), drop_outlier=True)
-
-        if model != 'corHMM':
-            # plot performance against signal for corHMM and a ML model
-            plot_signal_performance_two_models(bin_df, 'corHMM', model, 'binary', os.path.join(out_dir, 'models vs corHMM'))
-            plot_signal_performance_two_models(bin_df, 'corHMM', model, 'binary', os.path.join(out_dir, 'models vs corHMM'), drop_outlier=True)
-
-    for model in cont_model_names:
-        plot_signal_performance(cont_df, model, 'continuous', os.path.join(out_dir, 'continuous models', model))
-
-        if model != 'phylopars':
-            # plot performance against signal for corHMM and a ML model
-            plot_signal_performance_two_models(cont_df, 'phylopars', model, 'continuous', os.path.join(out_dir, 'models vs phylopars'))
-
-    #  phylogenetic imputation may perform poorly when lambda is less than 0.6 (Molina-Venegas et al., 2018)
-    ## compare models with signal above 0.6 and signal below 0.6
-    binary_above_06_df = bin_df[bin_df['Signal'] <= 0.4]
-    continuous_above_06_df = cont_df[cont_df['Signal'] >= 0.6]
-    plot_binary_and_continuous_cases(binary_above_06_df, continuous_above_06_df, 'Ev Model',
-                                     os.path.join(out_dir, 'strong signal'))
-    binary_above_06_df.describe(include='all').to_csv(os.path.join(out_dir, 'strong signal', 'binary_results_summary.csv'))
-    continuous_above_06_df.describe(include='all').to_csv(os.path.join(out_dir, 'strong signal', 'continuous_results_summary.csv'))
+    # # plot signal distribution
+    # sns.displot(bin_df.reset_index(drop=True), x='Signal', kind='kde')
+    # plt.savefig(os.path.join(out_dir, f'D.jpg'), dpi=300)
+    # plt.close()
+    #
+    # sns.displot(cont_df, x='Signal', kind='kde')
+    # plt.savefig(os.path.join(out_dir, f'lambda.jpg'), dpi=300)
+    # plt.close()
+    #
+    # ## plot performance against signal for each model
+    # for model in bin_model_names:
+    #     plot_signal_performance(bin_df, model, 'binary', os.path.join(out_dir, 'binary models', model))
+    #     plot_signal_performance(bin_df, model, 'binary', os.path.join(out_dir, 'binary models', model), drop_outlier=True)
+    #
+    #     if model != 'corHMM':
+    #         # plot performance against signal for corHMM and a ML model
+    #         plot_signal_performance_two_models(bin_df, 'corHMM', model, 'binary', os.path.join(out_dir, 'models vs corHMM'))
+    #         plot_signal_performance_two_models(bin_df, 'corHMM', model, 'binary', os.path.join(out_dir, 'models vs corHMM'), drop_outlier=True)
+    #
+    # for model in cont_model_names:
+    #     plot_signal_performance(cont_df, model, 'continuous', os.path.join(out_dir, 'continuous models', model))
+    #
+    #     if model != 'phylopars':
+    #         # plot performance against signal for corHMM and a ML model
+    #         plot_signal_performance_two_models(cont_df, 'phylopars', model, 'continuous', os.path.join(out_dir, 'models vs phylopars'))
+    #
+    # #  phylogenetic imputation may perform poorly when lambda is less than 0.6 (Molina-Venegas et al., 2018)
+    # ## compare models with signal above 0.6 and signal below 0.6
+    # binary_above_06_df = bin_df[bin_df['Signal'] <= 0.4]
+    # continuous_above_06_df = cont_df[cont_df['Signal'] >= 0.6]
+    # plot_binary_and_continuous_cases(binary_above_06_df, continuous_above_06_df, 'Ev Model',
+    #                                  os.path.join(out_dir, 'strong signal'))
+    # binary_above_06_df.describe(include='all').to_csv(os.path.join(out_dir, 'strong signal', 'binary_results_summary.csv'))
+    # continuous_above_06_df.describe(include='all').to_csv(os.path.join(out_dir, 'strong signal', 'continuous_results_summary.csv'))
 
     binary_below_06_df = bin_df[bin_df['Signal'] > 0.4]
     continuous_below_06_df = cont_df[cont_df['Signal'] < 0.6]
     plot_binary_and_continuous_cases(binary_below_06_df, continuous_below_06_df, 'Ev Model', os.path.join(out_dir, 'weak signal'))
     binary_below_06_df.describe(include='all').to_csv(os.path.join(out_dir, 'weak signal', 'binary_results_summary.csv'))
     continuous_below_06_df.describe(include='all').to_csv(os.path.join(out_dir, 'weak signal', 'continuous_results_summary.csv'))
+
+    ttests(binary_below_06_df, bin_model_names, os.path.join(out_dir, 'weak signal', 'binary'))
+    ttests(continuous_below_06_df, cont_model_names, os.path.join(out_dir, 'weak signal', 'continuous'))
 
 
 
